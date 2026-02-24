b8be242d60c8c508b4f5738ee006872f674ba4db
Gio <mathyoojr@gmail.com>
2026-02-24 09:35:31 -0500

    Added fuel shooting and scoring zone

--- Assets/Scripts/ScoreZone.cs
+++ Assets/Scripts/ScoreZone.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 
 public class ScoreZone : MonoBehaviour
 {
-    Collider col;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         col = GetComponent
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fuel"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem;
 public class SimpleMovement : MonoBehaviour
 {
     Rigidbody rb;
-    Vector3 velocity = new Vector3 (0f, 0f, 0f);
     GameObject fuelLauncher;
     [SerializeField] GameObject fuel;
     
@@ -18,22 +17,42 @@ public class SimpleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 velocity = new Vector3(0f, rb.linearVelocity.y, 0f);
         if (Input.GetKey(KeyCode.W))
         {
-            velocity.x = 10f;
+            velocity.x += 10f;
         }
-        else
+        if (Input.GetKey(KeyCode.S))
         {
-            velocity = Vector3.zero;
+            velocity.x -= 10f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.z += 10f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.z -= 10f;
+        }
+        Vector3 angularVelocity = new Vector3(0f, 0f, 0f);
+        if (Input.GetKey(KeyCode.Q))
+        {
+            angularVelocity.y -= 4f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            angularVelocity.y += 4f;
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject newFuel = Instantiate(fuel);
             newFuel.GetComponent<Rigidbody>().position = fuelLauncher.GetComponent<Rigidbody>().position;
-            newFuel.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 10f, 10f);
+            newFuel.GetComponent<Rigidbody>().linearVelocity = transform.rotation * new Vector3(5f, 15f, 0);
         }
             rb.linearVelocity = velocity;
+        rb.angularVelocity = angularVelocity;
     }
 
 }
